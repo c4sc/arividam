11e96a49aa7152ed3d2df12409877089df3df919
Arun Kumar <arunkakorp@gmail.com>
2016-06-25 22:28:41 +0530

    The template tag has_group returns True if the
    user is a superuser

--- arividam/users/templatetags/group_tags.py
+++ arividam/users/templatetags/group_tags.py
@@ -7,6 +7,8 @@ register = template.Library()
 
 @register.filter(name='has_group')
 def has_group(user, group_name):
+    if user.is_superuser:
+        return True
     try:
         group = Group.objects.get(name=group_name)
     except Group.DoesNotExist:
