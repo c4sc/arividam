377a0d4acd9a578146c0f02f518dbea502c8461f
Arun Kumar <arunkakorp@gmail.com>
2016-07-15 01:47:33 +0530

    Disable AliasPlugin since it can accidentally
    cause problems if an alias is placed within itself

--- arividam/siteconfig/apps.py
+++ arividam/siteconfig/apps.py
@@ -1,7 +1,18 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
+from cms.cms_plugins import AliasPlugin
+from cms.plugin_pool import plugin_pool
 
 
 class SiteconfigConfig(AppConfig):
     name = 'siteconfig'
+    verbose_name = "Site Configuration"
+
+    def ready(self):
+        def return_pass(self, r, p):
+            pass
+        AliasPlugin.get_extra_global_plugin_menu_items = return_pass
+        AliasPlugin.get_extra_placeholder_menu_items = return_pass
+        plugin_pool.unregister_plugin(AliasPlugin)
+
