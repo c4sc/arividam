60afb3084351621ea748a0a239df08451aca898a
Arun Kumar <arunkakorp@gmail.com>
2016-11-08 18:45:01 +0530

    Added query templatetag and used in notifications
    Filters only published non draft notifications in the main page

---
+++ arividam/siteconfig/templatetags/query.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+@register.assignment_tag
+def query(qs, **kwargs):
+    """ template tag which allows queryset filtering. Usage:
+          {% query books author=author as mybooks %}
+          {% for book in mybooks %}
+            ...
+          {% endfor %}
+    """
+    return qs.filter(**kwargs)
