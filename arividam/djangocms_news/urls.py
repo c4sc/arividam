53b2314058f265195e8aed62051ecc7b7acda6eb
Arun Kumar <arunkakorp@gmail.com>
2017-02-22 20:29:57 +0530

    Removed unused views from url list

--- arividam/djangocms_news/urls.py
+++ arividam/djangocms_news/urls.py
@@ -1,18 +1,18 @@
 from django.conf.urls import url
-from .views import ArticleList, ArticleDetail
+#from .views import ArticleList, ArticleDetail
 
 urlpatterns = [
-    url(r'^$', ArticleList.as_view(), name='index'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<pk>\d+)/$',
-        ArticleDetail.as_view(), name='article-detail'),
+#    url(r'^$', ArticleList.as_view(), name='index'),
+#    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<pk>\d+)/$',
+#        ArticleDetail.as_view(), name='article-detail'),
     # These support permalinks with <article_slug>
-    url(r'^(?P<slug>\w[-\w]*)/$',
-        ArticleDetail.as_view(), name='article-detail'),
-    url(r'^(?P<year>\d{4})/(?P<slug>\w[-\w]*)/$',
-        ArticleDetail.as_view(), name='article-detail'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<slug>\w[-\w]*)/$',
-        ArticleDetail.as_view(), name='article-detail'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>\w[-\w]*)/$',  # flake8: NOQA
-        ArticleDetail.as_view(), name='article-detail'),
+#    url(r'^(?P<slug>\w[-\w]*)/$',
+#        ArticleDetail.as_view(), name='article-detail'),
+#    url(r'^(?P<year>\d{4})/(?P<slug>\w[-\w]*)/$',
+#        ArticleDetail.as_view(), name='article-detail'),
+#    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<slug>\w[-\w]*)/$',
+#        ArticleDetail.as_view(), name='article-detail'),
+#    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>\w[-\w]*)/$',  # flake8: NOQA
+#        ArticleDetail.as_view(), name='article-detail'),
 
 ]
